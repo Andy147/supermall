91ef4ccc97d5bf55bada0e75a0ba1fa10db3741d
unknown <wlg118325338@163.com>
2020-04-25 00:49:51 +0800

    '初始化框架'

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tab_bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBar"
+    }
+</script>
+
+<style scoped>
+
+
+  #tab_bar{
+    display: flex;
+    position: fixed;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    height: 49px;
+    background-color: #f6f6f6;
+
+    box-shadow: 0px -2px 1px rgba(100,100,100,0.1);
+  }
+
+
+
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tab_bar_item" @click="itemClick">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <div :style="activeStye"><slot name="item-text"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "TabBarItem",
+      props:{
+        path:String,
+        activeColor:{
+          type:String,
+          default:'#d4237a'
+        }
+      },
+      methods:{
+        itemClick(){
+          this.$router.push(this.path);
+        }
+      },
+      computed:{
+        isActive(){
+          return this.$route.path.indexOf(this.path) !== -1;
+        },
+        activeStye(){
+          return this.isActive ? {color:this.activeColor}  : {};
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+  .tab_bar_item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab_bar_item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+
+    margin-bottom: 2px;
+  }
+  .active{
+    color: #d4237a;
+  }
+
+</style>
